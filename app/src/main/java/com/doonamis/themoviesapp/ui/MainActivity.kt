237f5d85aee9198502d230427aa07e6bba051c7c
DESKTOP-9Q3RS71\SRUIZSANCHEZ <sergio.ruiz1@amaris.com>
2022-04-28 16:17:45 +0200

    v2.0 branch created and changing to dark mode

--- app/src/main/java/com/doonamis/themoviesapp/ui/MainActivity.kt
+++ app/src/main/java/com/doonamis/themoviesapp/ui/MainActivity.kt
@@ -2,6 +2,12 @@ package com.doonamis.themoviesapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatDelegate
+import com.doonamis.themoviesapp.R
 import com.doonamis.themoviesapp.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -15,6 +21,51 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+    }
 
+    private fun openChooseThemeDialog() {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle(getString(R.string.choose_theme_title))
+        val options = arrayOf(getString(R.string.light_theme_text), getString(R.string.dark_theme_text), getString(R.string.system_default_theme_text))
+        val checkedOptions = 0
+
+        builder.setSingleChoiceItems(options, checkedOptions) { dialog, which ->
+
+            when (which) {
+                0 -> {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                    delegate.applyDayNight()
+                    dialog.dismiss()
+                }
+                1 -> {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                    delegate.applyDayNight()
+                    dialog.dismiss()
+                }
+                2 -> {
+                    AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM)
+                    delegate.applyDayNight()
+                    dialog.dismiss()
+                }
+            }
+        }
+
+        val dialog = builder.create()
+        dialog.show()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.toolbar_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+
+        when (item.itemId) {
+            R.id.action_change_theme -> openChooseThemeDialog()
+        }
+
+        return true
     }
 }
